a24fa65f1e24c81ef97846866234b47bd4811d17
spidyskip <antonio.morittu@icloud.com>
2023-12-14 16:51:46 +0100

    Update version

--- src/reddit_api/__init__.py
+++ src/reddit_api/__init__.py
@@ -8,11 +8,3 @@ from .tools import *
 # Define a function to return the version
 def get_version():
     return __version__
-
-# List of symbols to export
-__all__ = (
-    api.__all__ +
-    etl.__all__ +
-    tools.__all__ +
-    ['get_version']
-)
