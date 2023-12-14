c58611c1e68f3cff23ebbee38a36b5333042fc7e
anto <98389170+spidyskip@users.noreply.github.com>
2023-12-14 12:07:15 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='reddit_api',
     version='0.1',
-    packages=find_packages(where='src),
+    packages=find_packages(where='src'),
     description='Reddit API for Airflow',
     author='Unknwon',
     install_requires=[
