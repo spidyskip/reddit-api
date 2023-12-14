a8f88ee12b9dbb65fee9bc453f4413fc9d7b23bb
spidyskip <antonio.morittu@icloud.com>
2023-12-14 16:10:22 +0100

    add __versione__

--- setup.py
+++ setup.py
@@ -3,12 +3,26 @@ reddit-api
 """
 
 from setuptools import setup, find_packages
+import re
 
+NAME = 'reddit_api'
+PATH_MODULES = 'src/reddit_api'
 LONG_DESCRIPTION = 'reddit-api is a open source package that manipulate RedditAPI output and use it in ETL tasks'
 
+def get_version():
+    # Read the file and return the version
+    with open(f"{PATH_MODULES}/__init__.py") as f:
+        version = re.search(r'__version__ = "([^"]+)"', f.read())
+        if version:
+            return version.group(1)
+        else:
+            return None
+        
+# Use the get_version function to retrieve the version
+version = get_version()
 
 setup(
-    name='reddit_api',
+    name=NAME,
     version='0.3',
     packages=find_packages(where='src'),
     description='Reddit API for Airflow',
--- src/reddit_api/__init__.py
+++ src/reddit_api/__init__.py
@@ -1,3 +1,5 @@
+__version__ = "0.3"
+
 # Import necessary modules or packages here
 
 from .api import *
