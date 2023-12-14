cdc6fedc944068bd45a7a9b179b4a58e6e36ddb4
anto <98389170+spidyskip@users.noreply.github.com>
2023-12-14 11:16:03 +0100

    Create test_generator_id.py

---
+++ tests/test_generator_id.py
@@ -0,0 +1,23 @@
+# test_generator_id.py
+
+from datetime import datetime
+from reddit_api.generator_id import GeneratorID
+
+def test_id_number_generator():
+    generated_id = GeneratorID.id_number_generator()
+    assert isinstance(generated_id, int)
+
+def test_get_date():
+    generated_id = GeneratorID.id_number_generator()
+    generated_date = GeneratorID.get_date(generated_id)
+
+    current_date = datetime.fromtimestamp(generated_id)
+    assert generated_date == current_date
+
+def main():
+    test_id_number_generator()
+    test_get_date()
+    print("All tests passed successfully!")
+
+if __name__ == "__main__":
+    main()
